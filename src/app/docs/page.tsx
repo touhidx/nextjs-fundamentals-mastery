5d3229862a83b2e7b4b816014297ab035545f207
touhidx <touhidul0147@gmail.com>
2026-09-16 10:45:14 +0600

    learn about layout and creating links

---
+++ src/app/docs/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DocsPage = () => {
+  return (
+    <div>
+      <h1>This this the docs page</h1>
+    </div>
+  );
+};
+
+export default DocsPage;
---
+++ src/app/showcase/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ShowcasePage = () => {
+  return (
+    <div>
+      <h1>this is showcase page</h1>
+    </div>
+  );
+};
+
+export default ShowcasePage;
