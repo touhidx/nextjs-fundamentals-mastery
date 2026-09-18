7c2eda88d75956c4b349dcd46b8854156890bad8
touhidx <touhidul0147@gmail.com>
2026-09-18 12:36:26 +0600

    learn about image optimizitions

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    // https://images.pexels.com/photos/39390705/pexels-photo-39390705.jpeg
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        port: "",
+        pathname: "/**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,17 @@
+import Image from "next/image";
 import React from "react";
 
 const AboutPage = () => {
   return (
     <div>
       <h1>This this about AboutPage </h1>
+      <Image
+        src="https://images.pexels.com/photos/39390705/pexels-photo-39390705.jpeg"
+        width={400}
+        height={400}
+        alt="image"
+      ></Image>
+      <Image src="/image.png" width={400} height={400} alt="image"></Image>
     </div>
   );
 };
